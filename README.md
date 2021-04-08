5d77cb3527785186b90710988c9f6a7504d0c1ff
Rodrigo Torres <ratorres7@gmail.com>
2021-04-08 19:54:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hermione: A3 Internal Module Plugins
+
+This is a repository for all Eu.A3 to share their Data Science work.
+
+To start, just add your module to the "module_templates" folder and make a pull request.
+
+Remember to add a "_alpha" or "_beta" suffix to the folder name, so other users can know how mature your template is.
+
+All the templates added to this project will be available in `hermione add-module` cli command ( for everyone that have installed this module).
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import re
+import os
+
+exec(open('hermione_modules/_version.py').read())
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='hermione_ml_a3modules',
+    version=__version__,
+    author='A3Data',
+    author_email='hermione@a3data.com.br',
+    url='https://github.com/A3Data/hermione',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development'
+      ],
+      keywords='machine learning mlops devops artificial intelligence',
+      license='Apache License 2.0',
+    install_requires=[
+
+    ],
+    entry_points='''
+        [console_scripts]
+        hermione=hermione.cli:cli
+    ''',
+    python_requires='>=3.6'
+)
